1e094a250e76fc2a7430bcaf22df802036c0b5d6
Nate Mallison <healthynate@gmail.com>
2018-05-04 14:24:54 -0400

    learning lifecycle hooks

--- VueJS2Udemy/vueJSInstance/app.js
+++ VueJS2Udemy/vueJSInstance/app.js
@@ -89,6 +89,40 @@ const app3 = new Vue({
 
 app3.$mount('#app3');
 
-
+const app4 = new Vue({
+  el: '#app4',
+  data: {
+    title: 'Hello Vue!'
+  },
+  beforeCreate: function(){
+    console.log('beforeCreate()');
+  },
+  created: function(){
+    console.log('created()');
+  },
+  beforeMount: function(){
+    console.log('beforeMount()');
+  },
+  mounted: function(){
+    console.log('mounted()');
+  },
+  beforeUpdate: function(){
+    console.log('beforeUpdate()');
+  },
+  updated: function(){
+    console.log('updated()');
+  },
+  beforeDestroy: function(){
+    console.log('beforeDestroy()');
+  },
+  destroyed: function(){
+    console.log('destroyed()');
+  },
+  methods: {
+    destroy: function(){
+      this.$destroy();
+    }
+  }
+})
 
 
