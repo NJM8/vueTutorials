5b9f8a80ff94f55fb68d866808fff0c2e34bf4fe
Nate Mallison <healthynate@gmail.com>
2018-05-07 14:23:46 -0400

    sending data back to parent and prop validation

--- VueJS2Udemy/components/componentCommunication/src/components/UserDetail.vue
+++ VueJS2Udemy/components/componentCommunication/src/components/UserDetail.vue
@@ -2,13 +2,31 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User name: {{ name }}</p>
+    <p>User name: {{ switchName() }}</p>
+    <button @click="resetName">Reset name</button>
   </div>
 </template>
 
 <script>
-export default{
-  props: ['name']
+export default {
+  // prop validation
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    switchName: function(){
+      return this.name.split('').reverse().join('');
+    },
+    resetName: function(){
+      // note that this is not a good idea when using objects and arrays as they are passed by reference, doing this will break the reference
+      // this.name = 'Nate';
+      // I stopped using the above as it gave an error to never mutate the data from the child directly, but to pass and event up and let the parent mutate it, thus I am passing the data directly rather than this.name;
+      this.$emit('nameWasReset', 'Nate');
+    }
+  }
 }
 </script>
 
