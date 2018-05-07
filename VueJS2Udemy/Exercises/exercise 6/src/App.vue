11291a151476369908a2f33a15ebeb890fe452fe
Nate Mallison <healthynate@gmail.com>
2018-05-07 13:26:49 -0400

    added structure to folders

--- VueJS2Udemy/Exercises/exercise 6/src/App.vue
+++ VueJS2Udemy/Exercises/exercise 6/src/App.vue
@@ -9,9 +9,9 @@
 </template>
 
 <script>
-  import Header from './Header.vue'
-  import Footer from './Footer.vue'
-  import ServerList from './ServerList.vue'
+  import Header from './components/shared/Header.vue'
+  import Footer from './components/shared/Footer.vue'
+  import ServerList from './components/server/ServerList.vue'
   export default {
     components: {
       'my-header': Header,
