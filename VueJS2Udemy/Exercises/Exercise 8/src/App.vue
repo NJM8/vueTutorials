18b241e3912960d1a0a6763b1f7cdd54818faa63
Nate Mallison <healthynate@gmail.com>
2018-05-08 11:44:13 -0400

    finished exercise 8

---
+++ VueJS2Udemy/Exercises/Exercise 8/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="cmp='appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="cmp='appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="cmp='appRed'">Load Red Template</button>
+                <hr>
+                <component :is="cmp"><p>I'm a {{ getColor }} colored div.</p></component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+      data: function(){
+        return {
+          cmp: 'appBlue'
+        }
+      },
+      components: {
+          appBlue: Blue,
+          appGreen: Green,
+          appRed: Red
+      }, 
+      computed: {
+        getColor(){
+          return this.cmp.split(/([A-Z].*)/)[1];
+        }
+      }
+    }
+</script>
+
+<style>
+</style>
