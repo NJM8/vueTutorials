c59435cc239bea2ae7a4ebbcd8998c07f5b6667c
Nate Mallison <healthynate@gmail.com>
2018-05-08 10:42:35 -0400

    basic slots example

---
+++ VueJS2Udemy/components/slots/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <app-quote>
+    <!-- this html will get passed to the app-quote for rendering using a slot, now you can set the data on the elements here -->
+    <h2>{{ title }}</h2>
+    <p>A Quote</p>
+  </app-quote>
+</template>
+
+<script>
+import Quote from './components/Quote';
+
+export default {
+  data: function(){
+    return {
+      title: 'My Quotes'
+    }
+  },
+  components: {
+    'app-quote': Quote
+  }
+}
+</script>
+
+<style>
+/* this apply but can be overwritten by the child */
+h2 {
+  color: firebrick;
+}
+</style>
---
+++ VueJS2Udemy/components/slots/src/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- using slot to pass in html from the parent element -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+div {
+  border: 1px solid slategray;
+  box-shadow: 1px 1px 2px #42b983;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+  width: 65%;
+}
+/* note that styling is overwritten here */
+h2 {
+  color: #42b983;
+}
+</style>
+
