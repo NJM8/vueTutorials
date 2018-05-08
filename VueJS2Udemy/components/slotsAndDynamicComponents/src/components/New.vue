b0bf71e927c67be23ffb553c16e7df7089a9e27f
Nate Mallison <healthynate@gmail.com>
2018-05-08 11:11:19 -0400

    showing that dynamic components by default get destroyed when they are not in view

--- VueJS2Udemy/components/slotsAndDynamicComponents/src/components/New.vue
+++ VueJS2Udemy/components/slotsAndDynamicComponents/src/components/New.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h3>New Quote</h3>
+    <button @click="counter++">Add One</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
   export default {
-
+    data: function(){
+      return {
+        counter: 0
+      }
+    }
   }
 </script>
 
