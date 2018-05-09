5c2ebe536dbabb107a8b5d4c48a7dff16d3382db
Nate Mallison <healthynate@gmail.com>
2018-05-09 13:23:03 -0400

    showed how to use a local directive

--- VueJS2Udemy/directives/src/App.vue
+++ VueJS2Udemy/directives/src/App.vue
@@ -13,6 +13,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'slategrey'">Color This</p>
+                <p v-local-highlight:background.="'red'">Color This</p>
             </div>
         </div>
     </div>
@@ -20,6 +21,23 @@
 
 <script>
     export default {
+      directives: {
+        'local-highlight': {
+          bind(el, binding){
+            var delay = 0;
+            if (binding.modifiers['delayed']) {
+              delay = 3000;
+            }
+            setTimeout(() => {
+              if (binding.arg === 'background') {
+                el.style.backgroundColor = binding.value;
+              } else {
+                el.style.color = binding.value;
+              }
+            }, delay);
+          }
+        }
+      }
     }
 </script>
 
