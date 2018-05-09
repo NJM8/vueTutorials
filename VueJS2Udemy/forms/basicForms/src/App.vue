7c9e26593bc40ccec3174aa93074dfb43d34b85a
Nate Mallison <healthynate@gmail.com>
2018-05-09 10:10:18 -0400

    finished basic form controls

--- VueJS2Udemy/forms/basicForms/src/App.vue
+++ VueJS2Udemy/forms/basicForms/src/App.vue
@@ -11,21 +11,24 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="email">
+                                v-model="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
+                                <!-- lazy will only update the model when you click away from the element, you can also use .number to force a number or .trim to trim whilespace-->
                         <input
                                 type="password"
                                 id="password"
-                                class="form-control">
+                                class="form-control"
+                                v-model.lazy="userData.password">
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
                         <input
                                 type="number"
                                 id="age"
-                                class="form-control">
+                                class="form-control"
+                                v-model.number="userData.age">
                     </div>
 
                 </div>
@@ -33,27 +36,31 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="message">Message</label><br>
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work! Use v-model to have a placeholder-->
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <div class="form-group">
                         <label for="sendmail">
+                          <!-- when using checkboxes if you bind to the same model but have an array to store your data vue will process that and know to add each checked box to the array -->
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -62,26 +69,32 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="male">
+                      <!-- when using radio inputs you can bind to the same model and vue will recognize that you are using radio buttons and perform the proper actions, wow! -->
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
                     <label for="priority">Priority</label>
+                    <!-- you can dynamically create options for a dropdown by using a v-for, then set a default option by binding to selected, note it has to be ==. v-model is used here on the overall control instead of the input itself. You can also set a default selection with the v-model, this will override the :selected method of choosing a default-->
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option v-for="priority in priorities" 
+                                :selected="priority == 'Medium'">{{ priority }}</option>
                     </select>
                 </div>
             </div>
@@ -102,16 +115,16 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail: {{ email }}</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message: </p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriority }}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -124,7 +137,16 @@
     export default {
       data(){
         return {
-          email: ''
+          userData: {
+            email: '',
+            password: '',
+            age: 27
+          },
+          message: 'Tell us what you think',
+          sendMail: [],
+          gender: 'Male',
+          priorities: ['High', 'Medium', 'Low'],
+          selectedPriority: 'High'
         }
       }
     }
