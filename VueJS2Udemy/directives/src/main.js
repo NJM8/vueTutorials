3b64b5dd438871a7c681eed4b217e64e1f5f6a90
Nate Mallison <healthynate@gmail.com>
2018-05-09 13:20:13 -0400

    showed how to use a modifier

--- VueJS2Udemy/directives/src/main.js
+++ VueJS2Udemy/directives/src/main.js
@@ -12,11 +12,17 @@ Vue.directive('highlight', {
   bind(el, binding){
     // el.style.backgroundColor = '#42b883'
     // el.style.backgroundColor = binding.value;
-    if (binding.arg === 'background'){
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
     }
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 })
 
