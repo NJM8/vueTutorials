259023d92b0cea7b87a7df06b5797967f4be6833
Nate Mallison <healthynate@gmail.com>
2018-05-10 12:34:48 -0400

    updated name and added wrong answer view, added dynamically passed prop to question to generate new questions only if answer is correct

---
+++ VueJS2Udemy/courseProjects/The Super Quiz/src/components/WrongAnswer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="alert alert-warning text-center">
+    <h1>Wrong Answer!</h1>
+    <hr>
+    <button class="btn btn-primary" @click="tryAgain">Try again</button>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+  methods: {
+    tryAgain() {
+      this.$emit("confirmed");
+    }
+  }
+};
+</script>
