45c031156ad9aa06e5e70bce4a04c8774fee94c8
Nate Mallison <healthynate@gmail.com>
2018-05-10 10:59:34 -0400

    defined js hooks available for using js for animations

--- VueJS2Udemy/animationsAndTransitions/src/App.vue
+++ VueJS2Udemy/animationsAndTransitions/src/App.vue
@@ -30,6 +30,35 @@
           appear>
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
+        <br>
+        <h1>Multiple elements</h1>
+        <button class="btn btn-primary" @click="showMulti = !showMulti">Show Alert</button> 
+        <hr>       
+        <transition name="fade" mode="out-in">
+          <!-- if using multiple elements that only show at one time you need to provide a key otherwise Vue doesn't know which is which, you also must use v-if. use mode in-out or out-in to prevent the elements from sitting on top of each other as one animation starts and another ends -->
+          <div class="alert alert-info" v-if="showMulti" key="info">This is some Info</div>
+          <div class="alert alert-warning" v-else key="warning">This is some warning Info</div>
+        </transition>
+        <hr>
+        <h1>Using JS</h1>
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <br><br>
+        <!-- there are 8 hooks available to be used to attached your own js functions for animations. you can also mix them in with regular transitions if there is something you want do that depends on that transition state -->
+        <!-- the css false binding will tell vue not to look for css classes to bind -->
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false"
+          >  
+          <div class="myDiv" v-if="load"></div>
+        </transition>
       </div>
     </div>
   </div>
@@ -40,13 +69,53 @@ export default {
   data(){
     return {
       show: true,
-      alertAnimation: 'fade'
+      showMulti: true,
+      alertAnimation: 'fade',
+      load: true
     };
+  },
+  methods: {
+    beforeEnter(el){
+      console.log('beforeEnter')
+    },
+    enter(el, done){
+      console.log('enter');
+      // the done function tells Vue JS when this is done running, you must do this as it cannot sniff the JS like it can the CSS
+      done();
+    },
+    afterEnter(el){
+      console.log('afterEnter');
+    },
+    enterCancelled(el){
+      console.log('enterCancelled');
+    },
+    beforeLeave(el){
+      console.log('beforeLeave')
+    },
+    leave(el, done){
+      console.log('leave');
+      // the done function tells Vue JS when this is done running, you must do this as it cannot sniff the JS like it can the CSS
+      done();
+    },
+    afterLeave(el){
+      console.log('afterLeave');
+    },
+    leaveCancelled(el){
+      console.log('leaveCancelled');
+    }
   }
 };
 </script>
 
 <style>
+.myDiv {
+  width: 100px; 
+  height: 100px; 
+  background-color: #42b883;
+  border: 1px solid #35495e;
+  border-radius: 4px;
+}
+
 /* enter and leave final states are only active for one frame at the very start and end of the animation */
 .fade-enter {
   opacity: 0;
@@ -79,7 +148,7 @@ export default {
 }
 .slide-leave-active {
   animation: slide-out 700ms ease-out forwards;
-  transition: opacity 3s;
+  transition: opacity 1s;
   opacity: 0;
 }
 
