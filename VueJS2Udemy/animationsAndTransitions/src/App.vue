b8d37d7490bf95ec9a7b8a1f878dae1efe51bc1f
Nate Mallison <healthynate@gmail.com>
2018-05-10 10:25:39 -0400

    showed how to dynamically set the animation or transition class used by dynamically binding to some data

--- VueJS2Udemy/animationsAndTransitions/src/App.vue
+++ VueJS2Udemy/animationsAndTransitions/src/App.vue
@@ -4,6 +4,15 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr>
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br>
+        <transition :name="alertAnimation">
+          <div class="alert alert-info" v-show="show">This is some Info</div>
+        </transition>
+        <br>
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br><br>
         <transition name="fade">
@@ -30,7 +39,8 @@
 export default {
   data(){
     return {
-      show: true
+      show: true,
+      alertAnimation: 'fade'
     };
   }
 };
