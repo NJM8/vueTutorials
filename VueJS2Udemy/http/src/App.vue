9601d234b864708213cb0053f082ba2af539c093
Nate Mallison <healthynate@gmail.com>
2018-05-11 09:57:29 -0400

    used get to fetch and display data

--- VueJS2Udemy/http/src/App.vue
+++ VueJS2Udemy/http/src/App.vue
@@ -14,6 +14,18 @@
                 <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
+        <hr>
+        <div class="row">
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <button class="btn btn-primary" @click="fetchData">Get data</button>
+            <ul>
+              <li v-for="(user, index) in users" :key="index">
+                userName: {{ user.username }}
+                UserEmail: {{ user.email }}
+              </li>
+            </ul>
+          </div>
+        </div>
     </div>
 </template>
 
@@ -23,19 +35,32 @@
         return {
           user: {
             username: '',
-            email: ''
-          }
+            email: '',
+          },
+          users: []
         }
       },
       methods: {
         submit(){
-          console.log(this.user.username);
-          console.log(this.user.email);
-          this.$http.post("https://httpwithvueresource.firebaseio.com/data.json", this.user)
+          this.$http.post('', this.user)
             .then(res => {
               console.log(res);
             }, error => {
               console.log(error);
+            })
+        },
+        fetchData(){
+          this.$http.get('')
+            .then(res => {
+              console.log(res);
+              return res.json();
+            })
+            .then(data => {
+              const resArray = [];
+              for (const key in data){
+                resArray.push(data[key]);
+              }
+              this.users = resArray;
             })
         }
       }
--- VueJS2Udemy/http/src/main.js
+++ VueJS2Udemy/http/src/main.js
@@ -3,6 +3,8 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResource);
+// optional way to direct all requests to this route, then whatever is on the reqest will be appended
+Vue.http.options.root = "https://httpwithvueresource.firebaseio.com/data.json";
 
 new Vue({
   el: '#app',
