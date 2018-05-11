76de92bc7e7a4343de20cb010db8190145181e90
Nate Mallison <healthynate@gmail.com>
2018-05-11 09:27:20 -0400

    added vue resource and made a simple post request to a firebase backend

--- VueJS2Udemy/http/src/App.vue
+++ VueJS2Udemy/http/src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                  <label for="name">Username:</label>
+                  <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label for="name">Email:</label>
+                  <input type="email" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,26 @@
 
 <script>
     export default {
+      data(){
+        return {
+          user: {
+            username: '',
+            email: ''
+          }
+        }
+      },
+      methods: {
+        submit(){
+          console.log(this.user.username);
+          console.log(this.user.email);
+          this.$http.post("https://httpwithvueresource.firebaseio.com/data.json", this.user)
+            .then(res => {
+              console.log(res);
+            }, error => {
+              console.log(error);
+            })
+        }
+      }
     }
 </script>
 
