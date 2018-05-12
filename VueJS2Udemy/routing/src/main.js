85940fab9ee0eacd8cd2272ad24f4518ee70fd1a
Nate Mallison <healthynate@gmail.com>
2018-05-12 12:33:48 -0400

    testing history url

--- VueJS2Udemy/routing/src/main.js
+++ VueJS2Udemy/routing/src/main.js
@@ -6,6 +6,8 @@ import { routes } from './routes'
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  // history mode prevents the routes from being parsed on the server, the server will always serve the index.html and let the page handle the rest of the route, this is a way to have a nicer looking url, however this need to be setup correclty on the backend, the webpack vue cli does this for us.
+  mode: 'history',
   routes: routes
 })
 
