afc1cb7d29756cfcca953a30d4fe4011cdb09f3f
Nate Mallison <healthynate@gmail.com>
2018-05-14 10:00:57 -0400

    showed how to navigate to a path programmatically

--- VueJS2Udemy/routing/src/components/user/User.vue
+++ VueJS2Udemy/routing/src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button class="btn ptn-primary" @click="navigateHome">Back to Home</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      navigateHome(){
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
