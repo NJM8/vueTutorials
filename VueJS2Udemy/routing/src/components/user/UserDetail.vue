8a215faa9d640cec17c9dd976e7448f44af45547
Nate Mallison <healthynate@gmail.com>
2018-05-14 13:54:45 -0400

    showed how to use scroll behavior and hashes to navigate to a users previous position or controll scrolling in other ways

--- VueJS2Udemy/routing/src/components/user/UserDetail.vue
+++ VueJS2Udemy/routing/src/components/user/UserDetail.vue
@@ -2,12 +2,24 @@
   <div>
     <h3>Some User Details</h3>
     <p>User id: {{ id }}</p>
-    <router-link tag="button" class="btn btn-primary" :to="{ name: 'userEdit', query: { locale: 'en', q: 100 } }">Edit user</router-link>
+    <router-link tag="button" class="btn btn-primary" :to="link">Edit user</router-link>
   </div>
 </template>
 
 <script>
   export default {
+    data(){
+      return {
+        link: { 
+          name: 'userEdit', 
+          query: { 
+            locale: 'en', 
+            q: 100 
+          },
+          hash: '#data'
+        }
+      }
+    },
     props: {
       id: String
     }
--- VueJS2Udemy/routing/src/main.js
+++ VueJS2Udemy/routing/src/main.js
@@ -8,7 +8,16 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   // history mode prevents the routes from being parsed on the server, the server will always serve the index.html and let the page handle the rest of the route, this is a way to have a nicer looking url, however this need to be setup correctly on the backend, the webpack vue cli does this for us.
   mode: 'history',
-  routes: routes
+  routes: routes,
+  scrollBehavior(to, from, savedPosition){
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return {x: 0, y: 0}
+  }
 })
 
 new Vue({
