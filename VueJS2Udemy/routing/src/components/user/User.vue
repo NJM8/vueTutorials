37950aa51fd13bec7398484bef65698a0ae4ca77
Nate Mallison <healthynate@gmail.com>
2018-05-14 11:13:00 -0400

    showed how to send and use params

--- VueJS2Udemy/routing/src/components/user/User.vue
+++ VueJS2Udemy/routing/src/components/user/User.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>The User Page</h1>
+    <p>User id: {{ id }}</p>
     <hr>
     <button class="btn ptn-primary" @click="navigateHome">Back to Home</button>
   </div>
@@ -8,6 +9,11 @@
 
 <script>
   export default {
+    data(){
+      return {
+        id: this.$route.params.id
+      }
+    },
     methods: {
       navigateHome(){
         this.$router.push('/');
