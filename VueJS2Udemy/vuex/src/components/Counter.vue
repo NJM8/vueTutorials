9531b19bbdd026a98acad5122cb203f7cc31b648
Nate Mallison <healthynate@gmail.com>
2018-05-15 13:36:49 -0400

    showed how to modularize the store in case the file gets too large

--- VueJS2Udemy/vuex/src/components/Counter.vue
+++ VueJS2Udemy/vuex/src/components/Counter.vue
@@ -9,10 +9,10 @@
     export default {
         methods: {
             increment() {
-                this.$store.state.counter++;
+                this.$store.state.counter.counter++;
             },
             decrement() {
-                this.$store.state.counter--;
+                this.$store.state.counter.counter--;
             }
         }
     }
