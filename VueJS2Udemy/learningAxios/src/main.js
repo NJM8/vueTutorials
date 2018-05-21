16f97af20923c3db0798a94d438ea735db12fe6b
Nate Mallison <healthynate@gmail.com>
2018-05-21 14:34:10 -0400

    showed how to add and remove interceptors

--- VueJS2Udemy/learningAxios/src/main.js
+++ VueJS2Udemy/learningAxios/src/main.js
@@ -9,6 +9,20 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://learningaxios.firebaseio.com';
 axios.defaults.headers.common['authorization'] = 'superSecretKey';
 axios.defaults.headers.get['Accepts'] = 'application/json';
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('request');
+  console.log(config);
+  return config;
+});
+
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('response:');
+  console.log(res);
+  return res;
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
 
 new Vue({
   el: '#app',
