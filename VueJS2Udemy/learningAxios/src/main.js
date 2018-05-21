fa3703eef04f9b6c53dd84e3a017b34ecab329a1
Nate Mallison <healthynate@gmail.com>
2018-05-21 14:26:04 -0400

    showed how to set custom headers

--- VueJS2Udemy/learningAxios/src/main.js
+++ VueJS2Udemy/learningAxios/src/main.js
@@ -6,7 +6,9 @@ import store from './store'
 
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://learningaxios.firebaseio.com'
+axios.defaults.baseURL = 'https://learningaxios.firebaseio.com';
+axios.defaults.headers.common['authorization'] = 'superSecretKey';
+axios.defaults.headers.get['Accepts'] = 'application/json';
 
 new Vue({
   el: '#app',
