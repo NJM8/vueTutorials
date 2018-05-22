8e5dbd7e2ab2ee307fb1ef3828fda1db1f145a01
Nate Mallison <healthynate@gmail.com>
2018-05-22 11:54:47 -0400

    saved token and id in vuex

--- VueJS2Udemy/learningAxios/src/store.js
+++ VueJS2Udemy/learningAxios/src/store.js
@@ -11,7 +11,10 @@ export default new Vuex.Store({
     userId: null
   },
   mutations: {
-
+    authUser(state, userData){
+      state.idToken = userData.token;
+      state.userId = userData.userId;
+    }
   },
   actions: {
     signUp({commit}, authData){
@@ -20,7 +23,13 @@ export default new Vuex.Store({
         password: authData.password, 
         returnSecureToken: true
       })
-        .then(res => console.log(res))
+        .then(res => {
+          console.log(res);
+          commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId
+          });
+        })
         .catch(error => console.log(error))
 
     }, 
@@ -30,7 +39,13 @@ export default new Vuex.Store({
         password: authData.password, 
         returnSecureToken: true
       })
-        .then(res => console.log(res))
+        .then(res => {
+          console.log(res);
+          commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId
+          });
+        })
         .catch(error => console.log(error))
     }
   },
